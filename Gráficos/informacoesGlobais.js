f701a0412e943d75a042aff595498ed9098ffc2d
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2024-09-19 13:46:52 -0300

    rerer

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -4,7 +4,7 @@ async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)
-    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
+    const pessoasMundo = (dados.total_pessoas_mundo)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) *100)
     const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
