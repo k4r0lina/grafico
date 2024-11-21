23bd16532367969ee5fc8e34083726549ac39151
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2024-11-21 15:08:57 -0300

    ererere

--- Gráficos/pie.js
+++ Gráficos/pie.js
@@ -1,50 +1,60 @@
-import { getCSS } from "./common.js";
+import { criarGrafico, getCSS, incluirTexto } from "./common.js"
 
-async function redesFavoritasMundo() {
-  const url =
-    "https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json";
-  const res = await fetch(url);
-  const dados = await res.json();
-  const redes = ['A','B','C',]
-  const valores = ['20','40','40',]
-
-  // Criar o gráfico de pizza
-  const data = [
-    {
-      values: valores,
-      labels: redes,
-      type: "pie",
-      textinfo: "label+percent",
-    },
-  ];
-
-  // Configurar o layout do gráfico
-
-  const layout = {
-    plot_bgcolor: getCSS("--bg-color"),
-    paper_bgcolor: getCSS("--bg-color"),
-    title: {
-      text: "Redes sociais que os usuários mais gostam",
-      x: 0,
-      font: {
-        color: getCSS("--primary-color"),
-        family: getCSS("--font"),
-        size: 30,
-      },
-    },
-    legend: {
-      font: {
-        color: getCSS("--primary-color"),
-        size: 16,
-      },
-    },
-  };
-
-  // Inserir o gráfico na página
-  const grafico = document.createElement("div");
-  grafico.className = "grafico";
-  document.getElementById("graficos-container").appendChild(grafico);
-  Plotly.newPlot(grafico, data, layout);
+async function redesSociaisFavoritasMinhaEscola() {
+    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
+    if (dadosLocaisString) {
+        const dadosLocais = JSON.parse(dadosLocaisString)
+        processarDados(dadosLocais)
+    } else {
+        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+        const res = await fetch(url)
+        const dados = await res.json()
+        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
+        processarDados(dados)
+    }
 }
 
-redesFavoritasMundo();
+function processarDados(dados) {
+    const redesSociais = dados.slice(1).map(redes => redes[1])
+    const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
+        acc[redesSociais] = (acc[redesSociais] || 0) + 1
+        return acc
+    }, {})
+    const valores = Object.values(contagemRedesSociais)
+    const labels = Object.keys(contagemRedesSociais)
+
+    const data = [
+        {
+            values: valores,
+            labels: labels,
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
+        title: {
+            text: 'Redes sociais que as pessoas da minha escola mais gostam',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        legend: {
+            font: {
+                color: getCSS('--primary-color'),
+                size: 16
+            }
+        }
+    }
+
+    criarGrafico(data, layout)
+    incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>Instagram</span> em relação a outras redes.`)
+}
+
+redesSociaisFavoritasMinhaEscola()
