8ad0937aacd349e06ab706314e1d982d8b7e496c
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2024-11-21 15:15:31 -0300

    ewewewe

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -10,7 +10,6 @@ async function visualizarInformacoesGlobais() {
     const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `A região Nordeste é a que concentra o maior número de localidades quilombolas no Brasil, com <span> ${pessoasMundo} Quilombos </span> a região Sudeste vem em seguida, com <span> ${pessoasConectadas} quilombos. </span> Em relação à população quilombola, os estados da Bahia e Maranhão concentram metade da população quilombola do Brasil isso significa que são <span>${porcentagemConectada} % </span> de Quilombos. `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
