3dacf1933538c80427a804ac7f6812b7ab8f182c
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2024-11-21 15:28:59 -0300

    ssss

--- Gráficos/gr-1.js
+++ Gráficos/gr-1.js
@@ -53,7 +53,7 @@ async function quantidadeUsuarios() {
     grafico.className - 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
     Plotly.newPlot(grafico, data, layout)
-    incluirTexto(``)
+    incluirTexto(`aaaaaaaaaa`)
 
 }
 
